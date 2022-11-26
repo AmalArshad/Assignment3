43f74225ced41ce01213e301ec58c52909d349ca
Your Name <you@example.com>
2022-11-26 19:49:26 +0500

    Commit

---
+++ app/src/main/java/com/example/curdsqlite/CropImage.java
@@ -0,0 +1,22 @@
+package com.example.curdsqlite;
+
+import android.content.Intent;
+import android.net.Uri;
+
+public class CropImage {
+    public static int Crop_Image_Activity_Request_code;
+
+    public static ProcessBuilder activity() {
+        return null;
+    }
+
+    public static ActivityResult getActivityResult(Intent data) {
+        return null;
+    }
+
+    public class ActivityResult {
+        public Uri getUri() {
+            return null;
+        }
+    }
+}
